e7724198cafe6c3726282b32761bb5f8abe7ba98
DoSomethingStup <73495431+DoSomethingStupid@users.noreply.github.com>
2023-01-23 15:41:38 +0000

    new version

--- .ccls-cache/@home@runner@Project-1/main.c
+++ .ccls-cache/@home@runner@Project-1/main.c
@@ -1,18 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 
-int main () {
-	char dolorem[] = "Lorem ipsum dolor sit amet"; // sample text
-	char temp[100] = {}; //temporary empty string of length 99
-	int length = strlen(dolorem); // the length of sample text
-
-	strcpy(temp, dolorem); // copies dolorem text to temp for reversing
-	printf("Original string: %s\n", dolorem); // prints dolorem
-
-	for (int i = length; i > 0; i--) { // for length amount of times
-		dolorem[i-1] = temp[length - i]; // since the null character is included in the length, i needs to be subtracted by 1 when used for dolorem.
+int main() {
+	char s1[] = "Hello, world";
+	int len = strlen(s1) - 1;
+	while(len >= 0) {
+		printf("%c\n", s1[len]);
+		len--;
 	}
 
-	printf("Reversed string: %s\n", dolorem); // prints the reversed string
 	return 0;
 }
